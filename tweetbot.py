1af275b4fb01467e408cf506854488ae88470e5d
Matthias Bachfischer <bachfischer.matthias@googlemail.com>
2021-02-24 11:47:29 +0100

    minor update for deployment

--- tweetbot.py
+++ tweetbot.py
@@ -13,7 +13,7 @@ tweetbot = MarkovBot()
 # Get the current directory's path
 dirname = os.path.dirname(os.path.abspath(__file__))
 # Construct the path to the book
-book = os.path.join(dirname, u'Freud_Dream_Psychology.txt')
+book = os.path.join(dirname, u'smith_wealth_of_nations.txt')
 # Make your bot read the book!
 tweetbot.read(book)
 
@@ -27,7 +27,7 @@ tweetbot.read(book)
 # 	should attempt to use to start its text. It's nothing fancy: the bot will
 # 	simply try the first, and move on to the next if he can't find something
 # 	that works.
-my_first_text = tweetbot.generate_text(25, seedword=[u'dream', u'psychoanalysis'])
+my_first_text = tweetbot.generate_text(25, seedword=[u'labour', u'capital'])
 
 # Print your text to the console
 print(u'\ntweetbot says: "%s"' % (my_first_text))
@@ -58,14 +58,14 @@ tweetbot.twitter_login(cons_key, cons_secret, access_token, access_token_secret)
 
 # The target string is what the bot will reply to on Twitter. To learn more,
 # read: https://dev.twitter.com/streaming/overview/request-parameters#track
-targetstring = 'MarryMeFreud'
+targetstring = 'FamousQuotesFromRoding'
 # Keywords are words the bot will look for in tweets it'll reply to, and it
 # will attempt to use them as seeds for the reply
-keywords = ['marriage', 'ring', 'flowers', 'children', 'religion']
+keywords = ['business', 'money', 'public', 'business', 'liberty']
 # The prefix will be added to the start of all outgoing tweets.
 prefix = None
 # The suffix will be added to the end of all outgoing tweets.
-suffix = '#FreudSaysIDo'
+suffix = '#FamousQuotesFromRoding'
 # The maxconvdepth is the maximum depth of the conversation that the bot will
 # still reply to. This is relevant if you want to reply to all tweets directed
 # at a certain user. You don't want to keep replying in the same conversation,
@@ -73,11 +73,12 @@ suffix = '#FreudSaysIDo'
 # a shallow conversation depth. For example, a value of 2 will allow the bot
 # to only reply in conversations where there are two or less replies to the
 # original tweet.
-maxconvdepth = None
+maxconvdepth = 5
 
 # Start auto-responding to tweets by calling twitter_autoreply_start
 # This function operates in a Thread in the background, so your code will not
 # block by calling it.
+
 tweetbot.twitter_autoreply_start(targetstring, keywords=keywords, prefix=prefix, suffix=suffix, maxconvdepth=maxconvdepth)
  
 # Start periodically tweeting. This will post a tweet every X days, hours, and
@@ -85,12 +86,13 @@ tweetbot.twitter_autoreply_start(targetstring, keywords=keywords, prefix=prefix,
 # spam other people. Nobody likes spammers and trolls.)
 # This function operates in a Thread in the background, so your code will not
 # block by calling it.
-tweetbot.twitter_tweeting_start(days=0, hours=19, minutes=30, keywords=None, prefix=None, suffix='#BleepBloop')
+
+#tweetbot.twitter_tweeting_start(days=0, hours=19, minutes=30, keywords=None, prefix=None, suffix='#FamousQuotesFromRoding')
 
 # DO SOMETHING HERE TO ALLOW YOUR BOT TO BE ACTIVE IN THE BACKGROUND
-# You could, for example, wait for a week:
-secsinweek = 7 * 24 * 60 * 60
-time.sleep(secsinweek)
+while True: 
+    # Sleep for a minute
+    time.sleep(60)
  
 # Use the following to stop auto-responding
 # (Don't do this directly after starting it, or your bot will do nothing!)
